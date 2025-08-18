ec8b08e46485751640aab68ce5b5a188e740d747
The Remote Creative <57074293+theremotecreative@users.noreply.github.com>
2025-08-18 10:37:35 -0600

    fix: correct gatsby image queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,19 +12,16 @@ import AboutSection from "../components/home-components/about-section"
 const IndexPage = () => {
 
   const data = useStaticQuery(graphql`
-    query {
-        jasonThumb: file(relativePath: { eq: "Jason-Vanderheyden.jpg" }) {
-            childImageSharp {
-                gatsbyImageData(
-                  width: 800
-                  placeholder: TRACED_SVG
-                  formats: [AUTO, WEBP, AVIF]
-                )
-                fluid(maxWidth: 800, quality: 100) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
+    {
+      jasonThumb: file(relativePath: { eq: "Jason-Vanderheyden.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(
+            width: 800
+            placeholder: TRACED_SVG
+            formats: [AUTO, WEBP, AVIF]
+          )
         }
+      }
     }
   `)
 
@@ -36,10 +33,10 @@ const IndexPage = () => {
         keywords={"gatsby website development, Gatsby.js, WordPress, GSAP, GreenSock, Modern Web Development, Fast Websites, Headless CMS, A/B Testing, Split Testing"}
         ogTitle={"Home | The Remote Creative"} 
         ogDescription={"Modern Web Development with WordPress and Gatsby.js. Building lightning fast web apps with the latest react libraries and content management systems."}
-        ogImage={data.jasonThumb.childImageSharp.fluid}
+        ogImage={data.jasonThumb.childImageSharp.gatsbyImageData}
         twitterTitle={"Home | The Remote Creative"} 
         twitterDescription={"Modern Web Development with WordPress and Gatsby.js. Building lightning fast web apps with the latest react libraries and content management systems."}
-        twitterImage={data.jasonThumb.childImageSharp.fluid}
+        twitterImage={data.jasonThumb.childImageSharp.gatsbyImageData}
         />
       <HeroSection />
       <SectionTwo />
--- src/pages/project-portfolio.js
+++ src/pages/project-portfolio.js
@@ -8,19 +8,16 @@ import FeaturedPortfolio from "../components/featured-portfolio"
 const ProjectPage = () => {
 
   const data = useStaticQuery(graphql`
-    query {
-        jasonThumb: file(relativePath: { eq: "Jason-Vanderheyden.jpg" }) {
-            childImageSharp {
-                gatsbyImageData(
-                  width: 800
-                  placeholder: TRACED_SVG
-                  formats: [AUTO, WEBP, AVIF]
-                )
-                fluid(maxWidth: 800, quality: 100) {
-                  ...GatsbyImageSharpFluid_withWebp
-                }
-              }
+    {
+      jasonThumb: file(relativePath: { eq: "Jason-Vanderheyden.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(
+            width: 800
+            placeholder: TRACED_SVG
+            formats: [AUTO, WEBP, AVIF]
+          )
         }
+      }
     }
   `)
 
@@ -32,10 +29,10 @@ const ProjectPage = () => {
         keywords={"Gatsby.js, WordPress, GSAP, GreenSock, Modern Web Development, Fast Websites, Headless CMS, A/B Testing, Split Testing"}
         ogTitle={"Project Portfolio | The Remote Creative"} 
         ogDescription={"Modern Web Development with WordPress and Gatsby.js. View all of my latest projects and contact me if you're interested in working together."}
-        ogImage={data.jasonThumb.childImageSharp.fluid}
+        ogImage={data.jasonThumb.childImageSharp.gatsbyImageData}
         twitterTitle={"Project Portfolio | The Remote Creative"} 
         twitterDescription={"Modern Web Development with WordPress and Gatsby.js. View all of my latest projects and contact me if you're interested in working together."}
-        twitterImage={data.jasonThumb.childImageSharp.fluid}
+        twitterImage={data.jasonThumb.childImageSharp.gatsbyImageData}
       />
       <FeaturedPortfolio/>
     </SubLayout>
