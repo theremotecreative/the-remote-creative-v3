39d51dc72d3b69721cd7884c4255bfaaa912ef44
The Remote Creative <57074293+theremotecreative@users.noreply.github.com>
2025-08-26 11:40:48 -0500

    feat: highlight WordPress AI integrations on home page

--- src/components/home-components/hero-section.js
+++ src/components/home-components/hero-section.js
@@ -5,83 +5,86 @@ import ParticleBG from "./hero-bg"
 import HeroAnimation from "./hero-animation"
 
 const HeroSection = () => (
-    <HeroMain id="hero_main_bg" className="one-bg-change">
-        <ParticleBG/>
-        <div class="hero-content">
-            <h1>Senior Developers Supercharged with AI</h1>
-            <p>We combine years of engineering experience with cutting-edge artificial intelligence to build more.</p>
-        </div>
-                {/* <HeroAnimation/> */}
-    </HeroMain>
-  )
-  
-const HeroMain = styled.section`
-    background: #030e3b;
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    .hero-content {
-        position: relative;
-        max-width: 960px;
-        width: 100%;
-        margin: 0 auto;
-        margin-bottom: 0px;
-        padding: 0 30px;
-        z-index: 1;
-        transition-duration: .5s;
-        text-align: center;
-        h1 {
-            font-family: Roboto;
-            font-weight: 900;
-            font-size: 72px;
-            line-height: 1.2;
-            color: #fff;
-            max-width: 960px;
-            text-shadow: 2px 2px 4px rgba(0,0,0,.5);
-            transform: translateZ(60px);
-            transition-duration: .3s;
-            &:hover {
-                transform: translateZ(120px);
-            }
-        }
-        p {
-            font-family: Poppins;
-            font-weight: 400;
-            font-size: 36px;
-            line-height: 1.3;
-            max-width: 500px;
-            margin: 0 auto;
-            color: #fff;
-            transform: translateZ(60px);
-            text-shadow: 1px 1px 2px rgba(0,0,0,.5);
-        }
-    }
-    @media(max-width:960px) {
-        .hero-content {
-            text-align: center;
-            h1 {
-                font-size: 56px;
-            }
-            p {
-                margin: 0 auto;
-                font-size: 32px;
-            }
-        }
-    }
-    @media(max-width:767px) {
-        .hero-content {
-            text-align: center;
-            h1 {
-                font-size: 36px;
-            }
-            p {
-                margin: 0 auto;
-                font-size: 24px;
-            }
-        }
-    }
+  <HeroMain id="hero_main_bg" className="one-bg-change">
+    <ParticleBG />
+    <div class="hero-content">
+      <h1>Senior Developers Supercharged with AI</h1>
+      <p>
+        We combine years of engineering experience with cutting-edge artificial
+        intelligence to bring intelligent features to WordPress sites and modern
+        web apps.
+      </p>
+    </div>
+    {/* <HeroAnimation/> */}
+  </HeroMain>
+)
 
+const HeroMain = styled.section`
+  background: #030e3b;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  .hero-content {
+    position: relative;
+    max-width: 960px;
+    width: 100%;
+    margin: 0 auto;
+    margin-bottom: 0px;
+    padding: 0 30px;
+    z-index: 1;
+    transition-duration: 0.5s;
+    text-align: center;
+    h1 {
+      font-family: Roboto;
+      font-weight: 900;
+      font-size: 72px;
+      line-height: 1.2;
+      color: #fff;
+      max-width: 960px;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+      transform: translateZ(60px);
+      transition-duration: 0.3s;
+      &:hover {
+        transform: translateZ(120px);
+      }
+    }
+    p {
+      font-family: Poppins;
+      font-weight: 400;
+      font-size: 36px;
+      line-height: 1.3;
+      max-width: 500px;
+      margin: 0 auto;
+      color: #fff;
+      transform: translateZ(60px);
+      text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
+    }
+  }
+  @media (max-width: 960px) {
+    .hero-content {
+      text-align: center;
+      h1 {
+        font-size: 56px;
+      }
+      p {
+        margin: 0 auto;
+        font-size: 32px;
+      }
+    }
+  }
+  @media (max-width: 767px) {
+    .hero-content {
+      text-align: center;
+      h1 {
+        font-size: 36px;
+      }
+      p {
+        margin: 0 auto;
+        font-size: 24px;
+      }
+    }
+  }
 `
 
 export default HeroSection
--- src/components/home-components/section-two.js
+++ src/components/home-components/section-two.js
@@ -7,113 +7,131 @@ import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 const SectionTwo = () => {
+  useEffect(() => {
+    if (typeof window !== `undefined`) {
+      gsap.registerPlugin(ScrollTrigger)
+      gsap.core.globals("ScrollTrigger", ScrollTrigger)
+    }
 
-    useEffect(() => {
+    let sTwoTl = gsap.timeline({
+      paused: true,
+      scrollTrigger: {
+        trigger: "#section_two_bg",
+        start: "top 70%",
+        end: "top 70%",
+        id: "s2-background",
+        toggleActions: "play play reverse reverse",
+      },
+    })
+    sTwoTl.to(
+      ".one-bg-change",
+      { background: "#111", delay: 0.2, duration: 2 },
+      0,
+    )
+    sTwoTl.to(
+      ".two-bg-change",
+      { background: "#111", delay: 0.2, duration: 2 },
+      0,
+    )
+    sTwoTl.to(
+      ".three-bg-change",
+      { background: "#111", delay: 0.2, duration: 2 },
+      0,
+    )
+    sTwoTl.to(
+      ".four-bg-change",
+      { background: "#111", delay: 0.2, duration: 2 },
+      0,
+    )
+  }, [])
 
-        if (typeof window !== `undefined`) {
-            gsap.registerPlugin(ScrollTrigger)
-            gsap.core.globals('ScrollTrigger', ScrollTrigger)
-        }
-
-        let sTwoTl = gsap.timeline({
-            paused: true,
-            scrollTrigger: {
-                trigger: "#section_two_bg",
-                start: 'top 70%',
-                end: 'top 70%',
-                id: 's2-background',
-                toggleActions: "play play reverse reverse",
-                },
-            })
-        sTwoTl.to('.one-bg-change', { background: '#111', delay: 0.2, duration: 2 }, 0)
-        sTwoTl.to('.two-bg-change', { background: '#111', delay: 0.2, duration: 2 }, 0)
-        sTwoTl.to('.three-bg-change', { background: '#111', delay: 0.2, duration: 2 }, 0)
-        sTwoTl.to('.four-bg-change', { background: '#111', delay: 0.2, duration: 2 }, 0)
-    
-    }, []);
-
-    return(
+  return (
     <SectionMain id="section_two_bg" className="two-bg-change">
-        <div class="flex-row">
-            <div class="left-col">
-                <StrategySlider/>
-            </div>
-            <div class="right-col">
-                <h2>AI Strategies from Seasoned Developers</h2>
-                <p>Our senior devs blend deep experience with the latest AI techniques to deliver performance, speed, responsiveness, and analytics. These are some of the tools we use to keep every solution on the cutting edge.</p>
-                <p class="custom-button"><a href="#home_contact_row_one">Contact Us to Learn More</a></p>
-            </div>
+      <div class="flex-row">
+        <div class="left-col">
+          <StrategySlider />
         </div>
+        <div class="right-col">
+          <h2>AI Strategies from Seasoned Developers</h2>
+          <p>
+            Our senior devs blend deep experience with the latest AI techniques
+            to deliver performance, speed, responsiveness, and analytics. From
+            custom plugins to content generators, we integrate AI directly into
+            WordPress to keep every solution on the cutting edge.
+          </p>
+          <p class="custom-button">
+            <a href="#home_contact_row_one">Contact Us to Learn More</a>
+          </p>
+        </div>
+      </div>
     </SectionMain>
-    );
-
+  )
 }
 
 const SectionMain = styled.section`
-    background: #030e3b;
-    padding: 100px 20px;
+  background: #030e3b;
+  padding: 100px 20px;
+  .flex-row {
+    position: relative;
+    max-width: 1180px;
+    width: 100%;
+    margin: 0px auto;
+    display: flex;
+    align-items: center;
+    z-index: 1;
+  }
+  .left-col,
+  .right-col {
+    width: 50%;
+    padding: 40px;
+  }
+  h2 {
+    font-family: Roboto;
+    font-size: 60px;
+    font-weight: 700;
+    color: #fff;
+    line-height: 1.3;
+  }
+  p {
+    font-family: Poppins;
+    font-size: 20px;
+    font-weight: 400;
+    color: #fff;
+    line-height: 1.5;
+  }
+  .custom-button {
+    a {
+      color: #00ffff;
+      text-decoration: none;
+    }
+  }
+  @media (max-width: 1000px) {
     .flex-row {
-        position: relative;
-        max-width: 1180px;
-        width: 100%;
-        margin: 0px auto;
-        display: flex;
-        align-items: center;
-        z-index: 1;
+      flex-wrap: wrap;
     }
     .left-col,
     .right-col {
-        width: 50%;
-        padding: 40px;
+      width: 100%;
     }
+    .right-col {
+      order: 1;
+      text-align: center;
+    }
+    .left-col {
+      order: 2;
+    }
+  }
+  @media (max-width: 767px) {
     h2 {
-        font-family: Roboto;
-        font-size: 60px;
-        font-weight: 700;
-        color: #fff;
-        line-height: 1.3;
+      font-size: 36px;
     }
-    p {
-        font-family: Poppins;
-        font-size: 20px;
-        font-weight: 400;
-        color: #fff;
-        line-height: 1.5;
+  }
+  @media (max-width: 600px) {
+    .left-col,
+    .right-col {
+      padding: 0;
     }
-    .custom-button {
-        a {
-            color: #00ffff;
-            text-decoration: none;
-        }
-    }
-    @media(max-width:1000px) {
-        .flex-row {
-            flex-wrap: wrap;
-        }
-        .left-col,
-        .right-col {
-            width: 100%;
-        }
-        .right-col {
-            order: 1;
-            text-align: center;
-        }
-        .left-col {
-            order: 2;
-        }
-    }
-    @media(max-width:767px) {
-        h2 {
-            font-size: 36px;
-        }
-    }
-    @media(max-width:600px) {
-        .left-col,
-        .right-col {
-            padding: 0;
-        }
-    }
-
+  }
 `
 
 export default SectionTwo
